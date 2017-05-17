3ec0039d36f1fd12f3f8b728305e1cfb353b9958
Ammar Alammar <ammar@ammasa.net>
2017-05-18 00:50:40 +0300

    Fetch a hijri date from an API and display it
    Also a loading screen :)

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,38 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import styles from './App.scss';
+
+function parseDate(json: JSON) {
+  const date = json.data.gregorian;
+  const year = Number(date.year);
+  const month = Number(date.month.number) - 1;
+  const day = Number(date.day);
+
+  return new Date(year, month, day);
+}
+
+function Loading() {
+  return (
+    <div className={styles.loading}>
+      <span role="img" aria-label="Loading">🤔</span>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
+  <Loading />,
   document.getElementById('app'),
 );
+
+fetch('https://api.aladhan.com/hToG?date=01-09-1438')
+  .then(response => (
+    response.json()
+  ))
+  .then((json) => {
+    ReactDOM.render(
+      <h1>{parseDate(json).toLocaleString()}</h1>,
+      document.getElementById('app'),
+    );
+  });
