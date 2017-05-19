d4b94fe71bf3d700e9bfc6accd70abde715cee21
Ammar Alammar <ammar@ammasa.net>
2017-05-19 23:20:00 +0300

    Change the API endpoint & extract the date to a global variable

--- src/index.jsx
+++ src/index.jsx
@@ -5,13 +5,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import styles from './App.scss';
 
-function parseDate(json: JSON) {
-  const date = json.data.gregorian;
-  const year = Number(date.year);
-  const month = Number(date.month.number) - 1;
-  const day = Number(date.day);
+// You can change the date easily, formatting as `day/month/year`.
+const date = '1/9/1438';
 
-  return new Date(year, month, day);
+function parseInputDate() {
+  const parsedDate = date.split('/');
+
+  return JSON.stringify({
+    day: parsedDate[0],
+    month: parsedDate[1],
+    year: parsedDate[2],
+  });
+}
+
+function parseGregorianDate(json: JSON) {
+  return new Date(json.year_gr, json.month_gr - 1, json.day_gr);
 }
 
 function Loading() {
@@ -27,13 +35,17 @@ ReactDOM.render(
   document.getElementById('app'),
 );
 
-fetch('https://api.aladhan.com/hToG?date=01-09-1438')
+fetch('https://util.services/api/convert-hijri-to-greg/', {
+  method: 'POST',
+  headers: new Headers({ 'Content-Type': 'application/json' }),
+  body: parseInputDate(),
+})
   .then(response => (
     response.json()
   ))
   .then((json) => {
     ReactDOM.render(
-      <App date={parseDate(json)} />,
+      <App date={parseGregorianDate(json)} />,
       document.getElementById('app'),
     );
   });
